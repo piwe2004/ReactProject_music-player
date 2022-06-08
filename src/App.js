eea9204e44b4e1ccdc679af2a0245d5d4d2042f3
piwe2004 <piwe2004@gmail.com>
2022-06-08 14:19:48 +0900

    musicplayer - 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Controls from "./components/Controls/Controls";
+import PlayList from "./components/PlayList/PlayList";
+import ProgressArea from "./components/ProgrssArea/ProgressArea";
+import SongDetail from "./components/SongDetail/SongDetail";
+import React from "react";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" >
+        <SongDetail />
+        <ProgressArea />
+        <Controls />
+        <PlayList />
+      </div>
     </div>
   );
 }
