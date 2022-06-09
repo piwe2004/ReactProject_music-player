e9ff4646767337eee41e9415188251b74c6afbd5
piwe2004 <piwe2004@gmail.com>
2022-06-09 15:05:48 +0900

    musicplayer - 재생정지기능

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Controls from "./components/Controls/Controls";
 import PlayList from "./components/PlayList/PlayList";
 import ProgressArea from "./components/ProgrssArea/ProgressArea";
 import SongDetail from "./components/SongDetail/SongDetail";
-import React from "react";
+import React, {useRef} from "react";
 
 function App() {
+
+    const audioRef = useRef();
+    const onPlay = () => {
+        audioRef.current.play();
+    }
+    const onPause = () => {
+        audioRef.current.pause();
+    }
 
   return (
     <div className="App">
       <div className="container" >
         <SongDetail />
-        <ProgressArea />
-        <Controls />
+        <ProgressArea ref={audioRef} />
+        <Controls
+            play={onPlay}
+            pause={onPause}
+        />
         <PlayList />
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import {Provider} from "react-redux";
+import { createStore} from "redux"
+import musicPlayerReducer from "./store/musicPlayerReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
-
+const store = createStore(musicPlayerReducer, composeWithDevTools())
 
 ReactDOM.render(
   <React.StrictMode>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
